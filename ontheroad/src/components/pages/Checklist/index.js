cb64572d26956c1fe9a638790bfbc5a1f6dbddd2
Ivair Puerari <ivaair@hotmail.com>
2020-08-20 21:45:23 -0300

    trocado lib de step
    realizado alguns testes com a nova lib conforme documentação

--- ontheroad/src/components/pages/Checklist/index.js
+++ ontheroad/src/components/pages/Checklist/index.js
@@ -1,25 +1,54 @@
 import {useRoute} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {TouchableOpacity, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
-import {ProgressStep, ProgressSteps} from 'react-native-progress-steps';
+import StepIndicator from 'react-native-step-indicator';
+import {MaterialIcons} from 'react-native-vector-icons';
 import Feather from 'react-native-vector-icons/Feather';
 import Styles from './styles';
 
 export default function Checklist() {
-  const [isItemValido, setIsItemValido] = useState(false);
+  const labels = [
+    'Cart',
+    'Delivery Address',
+    'Order Summary',
+    'Payment Method',
+    'Track',
+  ];
+  const customStyles = {
+    stepIndicatorSize: 25,
+    currentStepIndicatorSize: 30,
+    separatorStrokeWidth: 2,
+    currentStepStrokeWidth: 3,
+    stepStrokeCurrentColor: '#fe7013',
+    stepStrokeWidth: 3,
+    stepStrokeFinishedColor: '#fe7013',
+    stepStrokeUnFinishedColor: '#aaaaaa',
+    separatorFinishedColor: '#fe7013',
+    separatorUnFinishedColor: '#aaaaaa',
+    stepIndicatorFinishedColor: '#fe7013',
+    stepIndicatorUnFinishedColor: '#ffffff',
+    stepIndicatorCurrentColor: '#ffffff',
+    stepIndicatorLabelFontSize: 13,
+    currentStepIndicatorLabelFontSize: 13,
+    stepIndicatorLabelCurrentColor: '#fe7013',
+    stepIndicatorLabelFinishedColor: '#ffffff',
+    stepIndicatorLabelUnFinishedColor: '#aaaaaa',
+    labelColor: '#999999',
+    labelSize: 13,
+    currentStepLabelColor: '#fe7013',
+  };
 
   const route = useRoute();
 
-  const [activeStep, setActiveStep] = useState(0);
+  const [currentPosition, setCurrentPosition] = useState(0);
 
   const frota = route.params.frota;
 
-  const [error, setError] = useState(false);
-
   useEffect(() => {
-    setActiveStep(0);
+    setCurrentPosition(0);
   }, []);
+
   const itemsFicticios = [
     {
       id: 1,
@@ -40,79 +69,63 @@ export default function Checklist() {
     },
   ];
 
-  async function onNextStep() {
-    if (!isItemValido) {
-      setIsItemValido(true);
-    } else {
-      setIsItemValido(false);
+  const getStepIndicatorIconConfig = ({position, stepStatus}) => {
+    const iconConfig = {
+      name: 'feed',
+      color: stepStatus === 'finished' ? '#ffffff' : '#fe7013',
+      size: 15,
+    };
+    switch (position) {
+      case 0: {
+        iconConfig.name = 'shopping-cart';
+        break;
+      }
+      case 1: {
+        iconConfig.name = 'location-on';
+        break;
+      }
+      case 2: {
+        iconConfig.name = 'assessment';
+        break;
+      }
+      case 3: {
+        iconConfig.name = 'payment';
+        break;
+      }
+      case 4: {
+        iconConfig.name = 'track-changes';
+        break;
+      }
+      default: {
+        break;
+      }
     }
-  }
+    return iconConfig;
+  };
+
+  const renderStepIndicator = (params) => (
+    <MaterialIcons {...getStepIndicatorIconConfig(params)} />
+  );
+
+  const onStepPress = (position) => {
+    setCurrentPosition(position);
+  };
 
   return (
     <View style={Styles.container}>
       <ScrollView>
-        <ProgressSteps
-          progressBarColor="#029B11"
-          activeStepIconBorderColor="#029B11"
-          activeLabelColor="#029B11"
-          completedProgressBarColor="#029B11"
-          completedStepIconColor="#029B11"
-          removeBtnRow={true}
-          activeStep={activeStep}>
-          <ProgressStep removeBtnRow label={itemsFicticios[0].title}>
-            <Text>{itemsFicticios[0].description}</Text>
-            <TouchableOpacity
-              onPress={() => {
-                setActiveStep(1);
-              }}>
-              <Feather name="arrow-right" size={40} color="#029B11" />
-            </TouchableOpacity>
-          </ProgressStep>
+        <StepIndicator
+          customStyles={customStyles}
+          currentPosition={currentPosition}
+          onPress={onStepPress}
+          renderStepIndicator={renderStepIndicator}
+          labels={labels}
+        />
 
-          <ProgressStep removeBtnRow label={itemsFicticios[1].title}>
-            <Text>{itemsFicticios[1].description}</Text>
-            <TouchableOpacity
-              onPress={() => {
-                setActiveStep(2);
-              }}>
-              <Feather name="arrow-right" size={40} color="#029B11" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                setActiveStep(0);
-                console.log(activeStep);
-              }}>
-              <Feather name="arrow-left" size={40} color="#029B11" />
-            </TouchableOpacity>
-          </ProgressStep>
-
-          <ProgressStep label={itemsFicticios[2].title}>
-            <Text>{itemsFicticios[2].description}</Text>
-          </ProgressStep>
-
-          <ProgressStep label="Finalizar">
-            <Text>Finalizado</Text>
-          </ProgressStep>
-        </ProgressSteps>
+        <TouchableOpacity onPress={onStepPress(currentPosition + 1)}>
+          <Feather name="arrow-left" size={40} color="white" />
+        </TouchableOpacity>
       </ScrollView>
     </View>
   );
 }
-
-const styleBtn = StyleSheet.create({
-  // ...
-  appButtonContainer: {
-    elevation: 8,
-    backgroundColor: '#029B11',
-    borderRadius: 10,
-    paddingVertical: 12,
-    paddingHorizontal: 12,
-  },
-  appButtonText: {
-    fontSize: 12,
-    color: '#fff',
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    textTransform: 'uppercase',
-  },
-});
